a4982f7e9b62d0568db9ab6df31a2d8495f10207
MechRalph04 <roxasraphael336@gmail.com>
2017-04-20 22:51:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTFILE HTML>
+<html>
+<head>
+  <title>MechRalph04 - Home</title>
+    <h1>Welcome!</h1>
+    <?php
+      $what = 'I do make websites, and make plugins, I also view stars, and I skate board.';
+      echo 'What do I do?', $what;
+    ?>
+</head>
+</html>
